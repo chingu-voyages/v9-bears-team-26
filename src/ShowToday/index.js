597150e7ac91c351eaaadad8ee382477cbdaf244
abidRahim <abid.rahim1@gmail.com>
2019-06-12 12:08:03 +0530

    Add CreateTopicForm component

---
+++ src/ShowToday/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import CreateTopicForm from '../CreateTopicForm';
+import './ShowToday.css';
+
+class ShowToday extends Component {
+  render() {
+    return (
+      <>
+        <CreateTopicForm />
+      </>
+    );
+  }
+}
+
+export default ShowToday;
