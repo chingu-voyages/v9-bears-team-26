f2c21903226ccd5f7881f0c158de58a365ca3c0d
arsts <arstsu@gmail.com>
2019-06-12 13:49:48 +0300

    created complonent setup

---
+++ src/components/PreviewCalendar/PreviewCalendar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PreviewCalendar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PreviewCalendar
